ada520943ded3bd8fa534000edd58942ed8c57e6
army3145 <76448948+army3145@users.noreply.github.com>
2021-02-05 20:06:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -64,3 +64,13 @@ for (var i = 0; i < names.length; i++) {
 }
 
 })();
+
+
+  (function( )  {
+  	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+       for (var  i  =  0;  i  <  names.length;  i++) {
+       console.log("Hello  "   +   names[i]);
+       }
+      
+      }( )); 
+
